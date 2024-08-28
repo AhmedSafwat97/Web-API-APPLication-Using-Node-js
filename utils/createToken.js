2abf49b8be60bd5183bf4e4f6b34378bdf9ab946
AhmedSafwat97 <xppahmad@gmail.com>
2024-08-28 22:34:33 +0300

    update the token time

--- utils/createToken.js
+++ utils/createToken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = process.env.JWT_SECRET; 
 
-const expiresIn = '20h';
+const expiresIn = '1000h';
 
 const createToken = (payload) =>
   jwt.sign( payload , secretKey , { expiresIn });
